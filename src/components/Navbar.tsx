cc9017d1a4c8bc2f11950dc3c8155cca43028a24
Sundharabalaji-K-L <narasimhanbalaji13@gmail.com>
2024-12-27 10:01:48 +0530

    build color theme switch

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,13 @@
-import {HStack, Image, Text} from "@chakra-ui/react";
+import {HStack, Image} from "@chakra-ui/react";
 import logo from '../assets/logo.webp'
+import ColourModeSwitch from "./ColourModeSwitch.tsx";
 
 const Navbar = ()=>{
     return (
         <div>
-            <HStack>
+            <HStack justifyContent="space-between" padding="10px">
                 <Image src={logo} boxSize="60px"/>
-                <Text>Dark mode</Text>
+                <ColourModeSwitch/>
             </HStack>
         </div>
     )
