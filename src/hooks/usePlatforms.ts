be20b49c6c1d5b52e26c223bcb70b14c1be1478e
Sundharabalaji-K-L <narasimhanbalaji13@gmail.com>
2024-12-30 15:51:31 +0530

    Customize theme

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,12 +1,12 @@
 import platforms from '../Data/platforms.ts'
 
 
-interface Plaform{
-    id: number;
-    name: string;
-    slug: string;
-}
-
+// interface Plaform{
+//     id: number;
+//     name: string;
+//     slug: string;
+// }
+//
 
 const usePlatforms = ()=> {
     return {data: platforms, isLoading: false, error: null}
