8bea45aadb5599fe17ea0bd706dbbc216cdd2c1b
Sundharabalaji-K-L <narasimhanbalaji13@gmail.com>
2024-12-30 11:34:07 +0530

    refactor: fixed loading state

--- src/hooks/useData.ts
+++ src/hooks/useData.ts
@@ -10,10 +10,11 @@ interface FetchResponse<T>{
 const useData = <T>(endpoint: string, requestConfig?: AxiosRequestConfig, deps?: any[])=>{
     const [data, setData] = useState<T[]>([]);
     const [error, setError] = useState('');
-    const [isLoading, setLoading] = useState(true);
+    const [isLoading, setLoading] = useState(false);
 
     useEffect(()=>{
         const controller = new AbortController();
+        setLoading(true);
         apiClient.get<FetchResponse<T>>(endpoint, {signal: controller.signal, ...requestConfig})
             .then(response => {
                 setData(response.data.results);
